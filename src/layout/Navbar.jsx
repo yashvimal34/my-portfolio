2912692189a29d555ee693a801746c83fb9a367a
Yash Vimal <yashsarthak79@gmail.com>
2026-01-11 11:44:02 +0530

    nav completed

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,23 +1,28 @@
 import {Button} from "@/components/Button";
+import {Menu, X} from "lucide-react";
+import { useState } from "react";
 
 const navLinks = [
     {href: "#about", label: "About"},
     {href: "#projects", label: "Projects"},
     {href: "#experience", label: "Experience"},
     {href: "#testimonials", label: "Testimonials"},
-    // {href: "#contact", label: "Contact"}
+    {href: "#contact", label: "Contact"}
 ];
 
 export const Navbar = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
                 <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
-                    PM<span className="text-primary">.</span>
+                    YASH<span className="text-primary">VIMAL</span>
                 </a>
 
                 {/* Desktop nav */}
-                <div className="flex items-center gap-1">
+                <div className="hidden md:flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) => (
                             <a href = {link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-muted-foregound hover:text-foreground rounded-full hover:bg-surface">
@@ -28,10 +33,30 @@ export const Navbar = () => {
                 </div>
 
                 {/* Contact button */}
-                <div>
+                <div className="hidden md:block">
                     <Button size="sm">Contact Me</Button>
                 </div>
+
+                {/* Mobile menu button */}
+                <button className="md:hidden p-2 text-foreground cursor-pointer"
+                onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
             </nav>
+
+            {/* Mobile Menu */}
+            {isMobileMenuOpen && (
+                <div className="md:hidden glass-strong animate-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                    {navLinks.map((link, index) => (
+                            <a href = {link.href} key={index} className="text-lg text-muted-foreground hover:text-foreground py-2">
+                                {link.label}
+                        </a>
+                        ))}
+                        <Button>Contact Me</Button>
+                </div>
+            </div>
+            )}
         </header>
     );
 };

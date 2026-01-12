13cd5b687761f7456dff8a26f813327c46d8f55f
Yash Vimal <yashsarthak79@gmail.com>
2026-01-12 23:40:25 +0530

    half hero section done

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     return (
-        <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+        <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
             <nav className="container mx-auto px-6 flex items-center justify-between">
                 <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
                     YASH<span className="text-primary">VIMAL</span>
